fed9a6be537e83d51f492c010b60929e2fff5be5
Timoté Vaucher <timote.vaucher@epfl.ch>
2019-04-13 13:09:00 +0200

    Update SVM

--- src/svm.py
+++ src/svm.py
@@ -4,44 +4,50 @@ from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import norm
 import math
 
-from utils import sign
 
 class SVM:
-    def __init__(self, learning_rate, lambda_reg, dim):
+    def __init__(self, learning_rate, lambda_reg, batch_frac, dim):
         self.__learning_rate = learning_rate
         self.__lambda_reg = lambda_reg
+        self.__batch_frac = batch_frac
         self.__dim = dim
         self.__persistence = 15
-        #self.__epsilon = 1e-4
-        # initialize weights
         self.__w = np.zeros(dim)
 
-    def fit(self, data, validation, max_iter, batch_size=100):
+    def fit(self, data, validation, max_iter):
         reached_criterion = False
         early_stopping_window = []
         window_smallest = math.inf
+        log = []
         for i in range(max_iter):
-            if(not reached_criterion) :
-                grad, train_loss = self.step(data.sample(False, 0.01))
-                self.__w += self.__learning_rate*grad.toarray().ravel()
-                #train_accuracy = self.predict(data) #, train_accuracy : {train_accuracy:.2f},
-                validation_loss = validation.map(
-                    lambda x: self.loss(x[0], x[1])).reduce(add)
-                validation_accuracy = self.predict(validation)
-                print(f'''iter : {i:3d}, avg_train_loss : {train_loss:.4f} validation_loss : {validation_loss:.2f},\
-                        validation_accuracy : {validation_accuracy}''')
+            if not reached_criterion:
+                # Compute gradient and train loss
+                grad, train_loss = self.step(
+                    data.sample(False, self.__batch_frac))
+                self.__w += self.__learning_rate * grad.toarray().ravel()
 
-                if(len(early_stopping_window) == self.__persistence) :
+                # Compute validation loss and accuracy
+                validation_loss = self.loss(validation)
+                validation_accuracy = self.predict(validation)
+
+                # Logging
+                log_iter = {'iter': i, 'avg_train_loss': train_loss,
+                            'validation_loss': validation_loss, 'validation_accuracy': validation_accuracy}
+                # print(log_iter)
+                log.append(log_iter)
+
+                # Early stopping criterion
+                if(len(early_stopping_window) == self.__persistence):
                     early_stopping_window = early_stopping_window[1:]
                     early_stopping_window.append(validation_loss)
                     if(min(early_stopping_window) > window_smallest):
                         reached_criterion = True
-                        print('reached early stopping criterion')
+                        log.append({'early_stop': True})
+                        break
                     window_smallest = min(early_stopping_window)
-                else :
+                else:
                     early_stopping_window.append(validation_loss)
-
-
+        return log
 
     def step(self, data):
         '''
@@ -60,12 +66,15 @@ class SVM:
             delta_w = self.__gradient(x, label)
         else:
             delta_w = self.__regularization_gradient(x)
-        return delta_w, self.loss(x, label, xw=xw)
+        return delta_w, self.loss_point(x, label, xw=xw)
 
-    def loss(self, x, label, xw=None):
+    def loss_point(self, x, label, xw=None):
         if xw is None:
             xw = x.dot(self.__w)[0]
         return max(1 - label * xw, 0) + self.__regularizer(x)
+
+    def loss(self, data):
+        return data.map(lambda x: self.loss_point(x[0], x[1])).reduce(add)
 
     def __regularizer(self, x):
         ''' Returns the regularization term '''
@@ -93,4 +102,4 @@ class SVM:
 
     def predict(self, data):
         ''' Predict the labels of the input data '''
-        return data.map(lambda x: sign(x[0].dot(self.__w)) == x[1]).reduce(add)/data.count()
+        return data.map(lambda x: np.sign(x[0].dot(self.__w)) == x[1]).reduce(add)/data.count()
