a4beb487a9c52ae0e4407cb4606b343275eecb2a
Timoté Vaucher <timote.vaucher@epfl.ch>
2019-04-14 15:07:50 +0200

    Add new params

--- src/settings.py
+++ src/settings.py
@@ -4,8 +4,8 @@ path = '..' if os.environ.get('LOCAL', False) else '/data'
 logpath = '../logs' if os.environ.get('LOCAL', False) else '/data/logs'
 
 dim = 47238
-learning_rate = 3.25
-lambda_reg = 1e-5
+learning_rate = 0.015
+lambda_reg = 1e-3
 batch_frac = 0.01
 
 validation_frac = 0.1
